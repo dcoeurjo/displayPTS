2a91ecd4b1bc5f5477daea5dedb23b257b80d6d4
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2021-01-27 14:22:53 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# displayPts
+# displayPTS
+
+`displayPTS` is a simple CLI tool to visualize tabulated ASCII
+pointsets with some quantities attached to the samples (scalars or
+vectors) using [polyscope](https://polyscope.run).
+
+For instance, if your pointset is an ascii file `data.pts`:
+```
+x y z scalar1 scalar2 vector1_x vector1_y vector1_z scalar3 ...
+(one line per point)
+```
+
+You can visualize the point set using:
+
+```
+./diplayPTS  -i data.pts --vectorCols 5
+```
+Each scalar columns will be considered as a `ScalarQuantity` in
+polyscope. `--vectorCols 5` means that columns 5 6 and 7 defines a vector quantity.
+
+
+
+
+# Building
+
+* Get the code (and its submodules):
+`git clone --recursive https://github.com/dcoeurjo/displayPTS.git`
+* Generate the project (cmake) and build the code. For instance:
+```
+cd displayPTS
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+```
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 
 void processFile(const std::string &filename,
-                const std::vector<unsigned int> &vectorsCol)
+                const std::vector<unsigned int> &vectorCols)
 {
   std::vector<std::array<double, 3>> pc;
   std::vector<std::vector<double>> alldata;
@@ -33,20 +33,20 @@ void processFile(const std::string &filename,
     alldata.push_back(data);
   }
   std::cout<<filename<<" "<<alldata[0].size()<<" cols ["
-           <<vectorsCol.size()<<" vectors, "
-           << alldata[0].size()-3*vectorsCol.size()-3<<" scalars]"<<std::endl;
+           <<vectorCols.size()<<" vectors, "
+           << alldata[0].size()-3*vectorCols.size()-3<<" scalars]"<<std::endl;
   //Polyscope
   auto ps = polyscope::registerPointCloud(filename, pc);
   
   unsigned int indexV;
   for(auto col=3u; col < alldata[0].size(); ++col)
   {
-    if (std::find(vectorsCol.begin(), vectorsCol.end(), col) == vectorsCol.end())
+    if (std::find(vectorCols.begin(), vectorCols.end(), col) == vectorCols.end())
     {
-      std::vector<double> H;
+      std::vector<double> scalars;
       for(auto i=0u; i < alldata.size(); ++i)
-        H.push_back(alldata[i][col]);
-      ps->addScalarQuantity("Scalar col"+std::to_string(col), H);
+        scalars.push_back(alldata[i][col]);
+      ps->addScalarQuantity("Scalar col"+std::to_string(col), scalars);
     }
     else
     {
@@ -65,8 +65,8 @@ int main(int argc, char **argv)
   std::vector<std::string> filenames;
   app.add_option("-i,--input,1", filenames, "Input point clouds")
      ->required();
-  std::vector<unsigned int> vectorsCol;
-  app.add_option("--vectors", vectorsCol, "Indices of columns to group as vectors (col col+1 col+2)");
+  std::vector<unsigned int> vectorCols;
+  app.add_option("--vectorCols", vectorCols, "Indices of columns to group as vectors (col col+1 col+2)");
   CLI11_PARSE(app,argc,argv);
   
   // Initialize polyscope
@@ -74,7 +74,7 @@ int main(int argc, char **argv)
 
   //Process all files
   for(auto &filename: filenames)
-    processFile(filename,vectorsCol);
+    processFile(filename,vectorCols);
 
   
   // Give control to the polyscope gui
